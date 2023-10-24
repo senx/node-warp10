ba9db56a5fd34b0357c34ba8db0ba010792673d8
Xavier Marin <xavier.marin@senx.io>
2023-10-24 17:11:18 +0200

    Update doc

---
+++ docs/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJySSxJDMjPzCtR0lEqSCzJAAol5yQWF6cW68Ol9DJKcnOA8tmZeSlKVoZGFrU6cP3hiUUFhgaYmiHimDpjAcgcaGF7AAAA"
--- docs/modules/DataPoint.md
+++
@@ -1,9 +0,0 @@
-[@senx/warp10](../README.md) / [Modules](../modules.md) / DataPoint
-
-# Module: DataPoint
-
-## Table of contents
-
-### Classes
-
-- [DataPoint](../classes/DataPoint.DataPoint.md)
